797652963442a1d5c41c92ed71c77cfd3abcb0c6
murphy701 <50907298+murphy701@users.noreply.github.com>
2022-05-29 17:55:30 +0900

    Create usercontroller.java

---
+++ src/main/java/bookstore/usercontroller.java
@@ -0,0 +1,33 @@
+package bookstore;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+@WebServlet("/usercontrol")
+public class usercontroller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	userDAO dao;
+	public void init(ServletConfig config) throws ServletException{
+		super.init(conifg);
+		dao=new userDAO();
+	}
+	
+	public String insert(HttpServletRequest request, HttpServletResponse response) {
+		userdb u= new userdb();
+		try {
+			BeanUtils.populate(u, request.getParameterMap());
+		}catch(Exception e) {e.printStackTrace();}
+		dao.insert(u);
+		return "login.jsp";
+	}
+}
