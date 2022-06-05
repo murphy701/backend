063f4543cb4ffca5c034e6a3029168732b41e8df
murphy701 <50907298+murphy701@users.noreply.github.com>
2022-06-05 15:41:57 +0900

    Update userDAO.java

--- src/main/java/bookstore/userDAO.java
+++ src/main/java/bookstore/userDAO.java
@@ -2,20 +2,24 @@ package bookstore;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 public class userDAO {
-	Connection conn=null;
-	PreparedStatement pstmt;
 	
+	Connection conn=null;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
 	final String JDBC_DRIVER="org.h2.Driver";
 	final String JDBC_URL="jdbc:h2:tcp://localhost/~/jwbookdb";
 	
-	public void open() {
+	public Connection open() {
 		try {
 			Class.forName(JDBC_DRIVER);
 			conn=DriverManager.getConnection(JDBC_URL, "jwbook", "1234");
 		}catch(Exception e) {e.printStackTrace();}
+		return conn;
 	}
+	
 	public void close() {
 		try {
 			pstmt.close();
@@ -24,14 +28,15 @@ public class userDAO {
 	}
 	
 	public void insert(userdb u) {
-		open();
+		Connection conn=open();
 		String sql=
-				"INSERT INTO userdb(name, id, call, address, email) values(?,?,?,?,?)";
+				"INSERT INTO userdb(id, pw, call, address, email) values(?,?,?,?,?)";
 		
 		try {
 			pstmt=conn.prepareStatement(sql);
-			pstmt.setString(1, u.getName());
-			pstmt.setString(2, u.getId());
+			
+			pstmt.setString(1, u.getId());
+			pstmt.setString(2, u.getPw());
 			pstmt.setString(3, u.getCall());
 			pstmt.setString(4, u.getAddress());
 			pstmt.setString(5, u.getEmail());
